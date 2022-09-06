1bdee1c091b29440f3c10f248e0298faffadb206
malena santellan <81625922+MaleSantellan@users.noreply.github.com>
2022-09-06 09:25:24 -0300

    Add files via upload

---
+++ TP2/js/inicial.js
@@ -0,0 +1,41 @@
+
+
+ // Or with jQuery
+
+ $(document).ready(function(){
+   $('.carousel').carousel();
+ });
+
+ $('.carousel.carousel-slider').carousel({
+     fullWidth: true
+   });
+
+
+// Or with jQuery
+
+$(document).ready(function(){
+$('.parallax').parallax();
+});
+
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+
+// Or with jQuery
+
+$(document).ready(function(){
+$('.slider').slider();
+});
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+$(document).ready(function(){
+   $('.modal').modal();
+ });
